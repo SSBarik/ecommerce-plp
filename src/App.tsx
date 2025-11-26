45f11cab6d9147db1f536bd7e99fe8eabad74e9f
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-11-26 12:31:41 +0530

    chore: setup apiClint, queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import "./App.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import { Button } from "./components/ui/button";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1 className="text-3xl font-bold underline">Ecommerce PLP</h1>
       <div className="flex flex-col items-center justify-center min-h-svh">
         <Button>Buy now</Button>
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
