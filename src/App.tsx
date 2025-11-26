07a8e57ceacb6693cbb6bb9c2a9c1c946efbdf5f
Shashank Shekhar Barik <ssbarik.sf@gmail.com>
2025-11-26 11:04:35 +0530

    chore: config shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Ecommerce PLP</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Ecommerce PLP</h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Buy now</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
